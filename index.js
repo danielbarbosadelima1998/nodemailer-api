0c0de9abd5df3dce00e84db87585ff1ea2b0e254
Daniel Barbosa de Lima <daniel.lima@ikatec.com.br>
2021-03-20 15:15:07 -0300

    feature(nodemailer): Implementa envio de email

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("./nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(bodyParser.json());
+
+var whitelist = ["http://localhost:3000", "http://localhost:3002", undefined];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+      console.log("quem esta tentando acessar: ",origin)
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.post("/send-email", async (req, res) => {
+  // validations
+//   if (validator.isEmail(req.body.to))
+//     return res.json({ error: true, messageError: "Email inválido!" });
+
+  // antes disso
+  const response = await nodemailer.sendMessage(req.body);
+  return res.json(response);
+});
+
+app.listen(3001);
---
+++ nodemailer.js
@@ -0,0 +1,48 @@
+const nodemailer = require("nodemailer");
+
+const errors = {
+  530: "Precisa se autenticar",
+  600: "Precisa de senha",
+  EENVELOPE: "Email do destinatário é invalido",
+};
+
+const getError = (e) => {
+  const error = errors[e.responseCode || e.code];
+  if (error) {
+    console.log("Message error --> ", error);
+    return error;
+  } else {
+    console.log("Message error --> ", e);
+    return "Erro desconhecido";
+  }
+};
+
+const initTransporter = () =>
+  nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "danielbarbosadelima1998@gmail.com",
+      pass: "sycecnbmdfnnpjou",
+    },
+  });
+
+const Nodemailer = {
+  sendMessage: async (data) => {
+    const transporter = initTransporter();
+
+    return transporter
+      .sendMail(data)
+      .then((result) => {
+        console.log("Message sent: %s", result.messageId);
+        return result;
+      })
+      .catch((e) => ({
+        error: true,
+        errorMessage: getError(e),
+      }));
+  },
+};
+
+module.exports = Nodemailer;
